3d4cc0343b283aa730ae8892315947d9223e8b24
anjali9050 <120652358+anjali9050@users.noreply.github.com>
2023-10-26 17:29:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ logging = logging.getLogger()
 
 
 bot = Client("bot",
-             bot_token=os.environ.get("BOT_TOKEN"),
-             api_id=int(os.environ.get("API_ID")),
-             api_hash=os.environ.get("API_HASH"))
-auth_users = [5908818236,5942085615]
+             bot_token=os.environ.get("BOT_TOKEN", "6056556039:AAHTGg-6rfDNkS-ZgWs65yrddTJXpyzyRtQ"),
+             api_id=int(os.environ.get("API_ID", "23560088")),
+             api_hash=os.environ.get("API_HASH", "999c01704d5c417749a98f4b8785fe88"))
+auth_users = [1864861524]
 sudo_users = auth_users
-sudo_groups = [-1001850795382]
+sudo_groups = [-100695481345]
 
 shell_usage = f"**USAGE:** Executes terminal commands directly via bot.\n\n<pre>/shell pip install requests</pre>"
 def one(user_id):
